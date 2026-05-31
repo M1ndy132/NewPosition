e72b0da63ea69dced9b5794e3d80abe92ed27a68
M1ndy <gdm1ndy234@gmail.com>
2026-05-30 21:57:33 -0500

    Added lives; reactor integrity.
    Added objective of collecting 10 energy cells to stabilize reactor.
    
    Added story.

--- main.py
+++ main.py
@@ -5,23 +5,33 @@ keywords = ["Boost", "Drain", "Amplify", "Dissipate"]
 playing = False
 loop_count = 0
 current_position = 0
+cells_collected = 0
+cells_to_be_collected = 10
+reactor_integrity = 3
 
 def confirm():
     global playing, loop_count
-    Confirmation = input("Would you like to continue playing? (y/n) ").strip().lower()
+    Confirmation = input("Gonna get the next one? (y/n) ").strip().lower()
     if Confirmation.startswith("y"):
         playing = True
         loop_count += 1
     else:
-        print("Thanks for playing!")
+        print("Thanks for for your help!")
         loop_count = 0
         playing = False
 
 
-Confirmation = input("Would you like to play? (y/n) ").strip().lower()
+print("\nDr. Infinity has disappeared.")
+print("Without him, the reactor has become unstable.")
+print("Energy cells have been scattered across the powerplant.")
+print("If we don't get them back soon and stabilize the reactor, The world we know may end.")
+
+Confirmation = input("\nWill you help us collect the energy cells? (y/n) ").strip().lower()
 if Confirmation.startswith("y"):
+    print("\nMay your calculations be true.")
     playing = True
 else:
+    print("\nWe understand the burden is too heavy.")
     playing = False
 
 while playing == True:
@@ -34,7 +44,10 @@ while playing == True:
         position = random.randint(1, 10)
     else:
         position = user_answer                                                      #type: ignore
-        
+
+    print(f"Reactor Integrity is at {reactor_integrity}/3")
+    print(f"Cells collected: {cells_collected}/{cells_to_be_collected}")    
+    
     print(f"\nYou are at position {position}")
     print(f"The energy cell was hit with a {keyword} {movement_int}")
     print("\nWhere do you need to be to collect it?")
@@ -52,11 +65,19 @@ while playing == True:
 
     if user_answer == correct_answer:   #type: ignore
         print("You got it!")
-        current_position = user_answer
-        confirm()
+        cells_collected += 1
+        if cells_collected == cells_to_be_collected:
+            print("You've stabalized the reactor. A job well done.")
+            break
+        else:
+            confirm()
     else:
         print(f"You missed it. The correct position was {correct_answer}") #type: ignore
-        current_position = user_answer
-        confirm()
+        reactor_integrity -= 1
+        if reactor_integrity == 0:
+            print("The reactor has failed. It's over for us all.")
+            break
+        else:
+            confirm()
     
     
